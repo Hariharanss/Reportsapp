8ed3bdd59e62c01bd994af4a85672df36f60a9fb
Hariharanss <hariwebhost01@gmail.com>
2019-02-26 17:21:23 +0530

    Server Js Updated

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('*', (req, res) => {
 });
 
 const http = require('http');
-const port = process.env.PORT || '3000';
+const port = process.env.PORT || '3001';
 app.set('port', port);
 const server = http.createServer(app);
 server.listen(port, () => console.log(`API running on localhost:${port}`));
