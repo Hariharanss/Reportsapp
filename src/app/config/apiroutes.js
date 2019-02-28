3b8fe0685fe9d32e80b0ca1a53788a36553ca29c
Hariharanss <hariwebhost01@gmail.com>
2019-02-28 16:40:17 +0530

    Code Updated

--- src/app/config/apiroutes.js
+++ src/app/config/apiroutes.js
@@ -91,7 +91,18 @@ router.post('/searchbasedoninputs', function (req, res) {
 
     fromdate = fromdate.split("-").reverse().join("-");
 
-    todate = todate.split("-").reverse().join("-");
+    todate = todate.split("-").reverse().join("-");     
+
+    if (projectname == '' && projectmodule == '' && developername == '' && fromdate == '' && todate == '') {
+        request.query('SELECT top 100 project_name as ProjectName, Pro_module as Module, Date as WorkingDate, Dailysheet_infor as Description, Cre_by as EmployeeName, [hours],[minu] FROM [ProjectMaster].[dbo].[Dailysheet] order by WorkingDate desc', function (err, rows) {
+            if (err) {
+                res.status(400).json(err);
+            } else {
+                res.send(rows.recordset);
+            }
+        });
+    }
+    else{
 
     var query = " SearchDailysheet '"+projectname+"','"+projectmodule+"','"+developername+"','"+fromdate+"','"+todate+"' ";
 
@@ -132,6 +143,7 @@ router.post('/searchbasedoninputs', function (req, res) {
             }
         }
     });
+}
 });
 
 
